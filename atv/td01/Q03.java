bcfbd59c1dc5b766d34e9151a72738f25e37e7f8
DalahanX <66420044+DalahanX@users.noreply.github.com>
2022-03-12 19:12:34 -0300

    Exercício - Q03 - v0.1
    Faça um programa que verifique (usando if e else) se uma letra digitada é “F” ou “M”. Conforme a letra escrever: F – Feminino, M- Masculino, Sexo inválido.

---
+++ atv/td01/Q03.java
@@ -0,0 +1,34 @@
+package td01;
+
+
+
+import java.util.Scanner;
+
+
+
+// 3ª) Faça um programa que verifique (usando if e else) se uma letra digitada é “F” ou “M”. Conforme a letra escrever: F – Feminino, M- Masculino, Sexo inválido.
+public class Q03 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        System.out.print("Digite seu sexo (M/F): ");
+        String sexo = teclado.nextLine().toUpperCase();
+
+
+        
+        switch (sexo) {
+            case "M":
+                System.out.println("Masculino.");
+                break;
+
+            case "F":
+                System.out.println("Feminino.");
+                break;
+
+            default:
+                System.out.println("[ERRO] " + sexo + " não existe!");
+        }
+    }
+}
