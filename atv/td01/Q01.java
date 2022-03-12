fca972779ccef9cb1b899c1280ed9976af2cada5
DalahanX <66420044+DalahanX@users.noreply.github.com>
2022-03-12 19:12:02 -0300

    Exercício Q01 - v0.1
    Faça um programa que peça dois números e verifique (usando if e else) e imprima o maior deles.

---
+++ atv/td01/Q01.java
@@ -0,0 +1,38 @@
+package td01;
+
+
+
+import java.util.Scanner;
+
+
+
+// 1ª) Faça um programa que peça dois números e verifique (usando if e else) e imprima o maior deles.
+public class Q01 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        System.out.print("Digite um número: ");
+        float n1 = teclado.nextFloat();
+
+        System.out.print("Digite outro: ");
+        float n2 = teclado.nextFloat();
+
+
+
+        System.out.print("\n" + n1);
+        if (n1 > n2) {
+            System.out.print(" é maior que ");
+        }
+
+        else if (n1 < n2) {
+            System.out.print(" é menor que ");
+        }
+
+        else {
+            System.out.print(" é igual à ");
+        }
+        System.out.print(n2 + "!");
+    }
+}
