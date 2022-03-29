6ca43f852c9d402f266e0fbd8a382cda8fb8388b
Miguel <miguellalves9@gmail.com>
2022-03-28 22:30:42 -0300

    Exercício Q19 - v0.1
    Faça um programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número e:
    
    - Par ou ímpar;
    - Positivo ou negativo.

---
+++ atv/td01/Q19.java
@@ -0,0 +1,88 @@
+package td01;
+
+
+
+import java.util.Scanner;
+
+
+
+/* 19ª) Faça um programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número e:
+
+- Par ou ímpar;
+- Positivo ou negativo.
+*/
+public class Q19 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        System.out.print("Digite um valor: ");
+        double n1 = teclado.nextDouble();
+
+        System.out.print("Agora outro: ");
+        double n2 = teclado.nextDouble();
+
+
+
+        System.out.print("\n\n\nAgora a operação:\n\n[ 1 ] - Soma\n[ 2 ] - Divisão\n[ 3 ] - Subtração\n[ 4 ] - Multiplicação\n\n-> ");
+        int op = teclado.nextInt();
+
+
+
+        double res = 0;
+        switch (op) {
+            case 1:
+                res = n1 + n2;
+                break;
+
+            case 2:
+                res = n1 / n2;
+                break;
+
+            case 3:
+                res = n1 - n2;
+                break;
+
+            case 4:
+                res = n1 * n2;
+                break;
+        }
+
+
+
+        if (op >= 1 && op <= 4) {
+            System.out.print("\n\n\nO resultado da operação é de " + res + " e deu num número ");
+
+
+
+            // Verifica o sinal do número;
+            if (res > 0) {
+                System.out.print("positivo e ");
+            }
+
+            else if (res < 0) {
+                System.out.print("negativo e ");
+            }
+
+            else {
+                System.out.print("nulo e ");
+            }
+
+
+
+            // Verifica se o número é par ou ímpar;
+            if (res % 2 == 0) {
+                System.out.print("par!");
+            }
+
+            else {
+                System.out.print("ímpar!");
+            }
+        }
+
+        else {
+            System.out.println("\n[ERRO] Operação Inválida!");
+        }
+    }
+}
