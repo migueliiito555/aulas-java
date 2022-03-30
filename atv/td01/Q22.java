f32742c6d2363d4f727e88d7f62295210ed42c8b
Miguel <miguellalves9@gmail.com>
2022-03-29 21:19:43 -0300

    Exercício Q22 - v0.1
    Faça um programa que peça dois números ao usuário e mostre qual o maior e qual o menor.

---
+++ atv/td01/Q22.java
@@ -0,0 +1,26 @@
+package td01;
+
+
+
+import java.util.Scanner;
+
+
+
+// 22ª) Faça um programa que peça dois números ao usuário e mostre qual o maior e qual o menor;
+public class Q22 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        System.out.print("Digite um número: ");
+        double n1 = teclado.nextDouble();
+
+        System.out.print("Mais um: ");
+        double n2 = teclado.nextDouble();
+
+
+
+        System.out.println("\nMaior: " + Math.max(n1, n2) + "\nMenor: " + Math.min(n1, n2));
+    }
+}
