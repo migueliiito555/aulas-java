2fc3b64e4bc5e9863a8c50c5af1274806eda903e
Miguel <miguellalves9@gmail.com>
2022-04-27 20:31:02 -0300

    Exercício Q01b - v0.1
    1ª) Para cada conjunto de valores abaixo, escreva o código Java, usando laço(s), que preencha um array com os valores:
    
    b) 0 1 4 9 16 25 36 49 64 81 100

---
+++ atv/td03/Q01b.java
@@ -0,0 +1,16 @@
+package td03;
+
+
+// 1B
+public class Q01b {
+    public static void main(String[] args) {
+        int vetor[] = new int[11];
+
+
+
+        for (int i = 0; i <= 10; i++) {
+            vetor[i] = i * i;
+            System.out.println(vetor[i]);
+        }
+    }
+}
