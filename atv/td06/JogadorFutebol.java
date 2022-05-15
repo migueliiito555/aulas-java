98b32f4e6ec220d98c413521076c985770d005b6
Miguel <miguellalves9@gmail.com>
2022-05-15 17:00:24 -0300

    Exercício Q03 - v0.1
    - Troca de diretório.

--- atv/td06/JogadorFutebol.java
+++ atv/td06/JogadorFutebol.java
@@ -1,4 +1,4 @@
-package td06.Q03;
+package td06;
 
 // 3ª) Crie uma classe para representar um jogador de futebol, com os atributos nome, posição, data de nascimento, nacionalidade, altura e peso. Crie os métodos públicos necessários para sets e gets e também um método para imprimir todos os dados do jogador. Crie um método para calcular a idade do jogador e outro método para mostrar quanto tempo falta para o jogador se aposentar. Para isso, considere que os jogadores da posição de defesa se aposentam em média aos 40 anos, os jogadores de meio-campo aos 38 e os atacantes aos 35.
 public class JogadorFutebol
